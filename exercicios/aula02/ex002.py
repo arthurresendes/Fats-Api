fbfb36707ed7213f291ba81ff3df6c33e7410204
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-22 15:04:50 -0300

    atualização

--- exercicios/aula02/ex002.py
+++ exercicios/aula02/ex002.py
@@ -5,9 +5,13 @@ app = FastAPI()
 auth_routes = APIRouter(prefix="/auth", tags=["auth"])
 order_routher = APIRouter(prefix="/pedidos", tags=["pedidos"])
 
-@order_routher.get('/')
-async def 
+@order_routher.get('/listagem')
+async def lista():
+    return {"Lista produtos": "[Café, Coca-cola, Banana]"}
 
+@auth_routes.post("/pedidos")
+async def cadastrar(produto: str , quantidade: int):
+    return {"Produto": {produto}, "Quantidade": {quantidade}}
 
 app.include_router(auth_routes)
 app.include_router(order_routher)
