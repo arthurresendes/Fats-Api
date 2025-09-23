5f0c0db416dd731e8e2042ef5a108a90132420ae
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-23 19:53:06 -0300

    atualizar

--- Curso/aulas/models.py
+++ Curso/aulas/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, String , Integer, Boolean , Float , ForeignKey
 from sqlalchemy.orm import declarative_base
 
 # Cria a conexão com o banco
@@ -8,6 +8,14 @@ db = create_engine("sqlite:///banco.db")
 Base = declarative_base()
 
 # Cria as classes/tabelas do banco
-
+class Usuario(Base):
+    __tablename__ = "usuarios"
+    
+    id = Column("id", Integer , primary_key=True, autoincrement=True)
+    nome = Column("nome", String)
+    email = Column("email" , String, nullable=False)
+    senha = Column("senha" , String)
+    ativo = Column("ativo" , Boolean)
+    admin = Column("admin" , Boolean, default=False)
 
 # executa a criação
