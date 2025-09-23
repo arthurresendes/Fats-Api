7e7bd24c33539f0594b3605c67e468b2a15ca94c
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-23 20:33:18 -0300

    atualização

--- Curso/aulas/alembic/env.py
+++ Curso/aulas/alembic/env.py
@@ -7,7 +7,7 @@ from alembic import context
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "main.py")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,7 +22,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from models import Base
+
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
