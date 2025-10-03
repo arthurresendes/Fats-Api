683f3d3f47e3dd5acd3621e4cf95b96db356f0e5
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-03 17:02:35 -0300

    atualizar

--- exercicios/Endpoints/alunos.py
+++ exercicios/Endpoints/alunos.py
@@ -5,7 +5,8 @@ app = FastAPI()
 
 class Aluno(BaseModel):
     nome: str
-    idade: int
+    nota1: float
+    nota2: float
 
 alunos = []
 contador_alunos = 1
@@ -13,7 +14,7 @@ contador_alunos = 1
 @app.post("/cadastro")
 async def cadastro(aluno: Aluno):
     global contador_alunos
-    novo_aluno = {"id": contador_alunos, "nome": aluno.nome , "idade": aluno.idade}
+    novo_aluno = {"id": contador_alunos, "nome": aluno.nome , "nota_1": aluno.nota1, "nota_2": aluno.nota2}
     alunos.append(novo_aluno)
     contador_alunos+=1
     return novo_aluno
@@ -21,3 +22,19 @@ async def cadastro(aluno: Aluno):
 @app.get("/verifica")
 def listar_alunos():
     return alunos
+
+@app.get("/verifica/nota/{id}")
+async def nota(id: int):
+    for i in alunos:
+        if i['id'] == id:
+            try:
+                soma = float(i['nota_1']) + float(i['nota_2'])
+                media = soma / 2
+                return {
+                    "id": i['id'],
+                    "nome": i['nome'],
+                    "media": media
+                }
+            except Exception as e:
+                return {"erro": f"problema ao calcular média: {str(e)}"}
+    return {"mensagem": "aluno_nao_encontrado"}
