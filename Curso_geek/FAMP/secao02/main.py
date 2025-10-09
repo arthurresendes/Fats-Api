ca00736a4b3c0a42a85990a2e09dac0169a3285b
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-09 14:54:54 -0300

    atualização

---
+++ Curso_geek/FAMP/secao02/main.py
@@ -0,0 +1,22 @@
+'''
+Como criamos um ambiente virtual:
+
+1-Usar a opção no proprio VSCODE:
+Instalamos:
+pip install virtualenvwrapper-win
+
+Criando o espaço virtual:
+python -m venv famp-secao02      
+
+Ativar (Windows)
+famp-secao02\Scripts\activate
+
+2-Fazer internamente:
+
+mkvirtualenv famp-secao02
+
+Para entrar no ambiente virtual
+workon famp-secao02 
+Caso não funcione coloque o caminho da pasta:
+& "C:\Users\<usuario>\Envs\famp-secao02\Scripts\Activate.ps1"           
+'''
