8b72458e00b7bfb7ac5c8e9d17b7f1f29473a0d2
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-13 16:38:41 -0300

    atualização

--- Curso_geek/FAMP/secao03/main.py
+++ Curso_geek/FAMP/secao03/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
-from fastapi import status, HTTPException
+from fastapi import status, HTTPException,Response,Path,Query,Header,Depends
 from models import Curso
+from typing import Optional, Any,Dict,List
+from time import sleep
 # & "C:\Users\arregomes\Envs\famp-secao03\Scripts\Activate.ps1"
-app = FastAPI()
+def fake_db():
+    try:
+        print("Abrindo conexão com o banco de dados")
+        sleep(1)
+    finally:
+        print("Fechando conexão com banco de dados!!")
+        sleep(2)
+
+
+app = FastAPI(title="Aprendizado Fast Api",
+              version="0.0.1",
+              description="Uma API para estudo do FastApi"
+              )
 
 cursos = {
     1:{
@@ -17,12 +31,12 @@ cursos = {
     }
 }
 
-@app.get('/cursos')
-async def get_cursos():
+@app.get('/cursos', description="Retorna todos os cursos ou lista vazia", summary="Retorna todos os cursos", response_model=Dict[int,Curso], response_description="Cursos encontrados")
+async def get_cursos(db: Any = Depends(fake_db)):
     return cursos
 
 @app.get('/cursos/{curso_id}')
-async def get_cursos_id(curso_id:int):
+async def get_cursos_id(curso_id:int = Path(default=None, title="ID do curso", description="Deve ser entre 1 e 2", gt=0, lt=3)):
     try:
         curso = cursos[curso_id]
         curso.update({"id": curso_id})
@@ -37,6 +51,31 @@ async def post_curso(curso: Curso):
     del curso.id
     return curso
 
+@app.put("/cursos/{curso_id}")
+async def put_curso(curso_id: int,curso: Curso):
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        curso.id = curso_id
+        return curso
+    else:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Não existe um curso com o id {curso_id}")
+
+@app.delete("/cursos/{curso_id}")
+async def delete_curso(curso_id:int):
+    if curso_id in cursos:
+        del cursos[curso_id]
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id não encontrado")
+
+@app.get("/calculadora")
+async def calcular(a:int = Query(default=None, gt=5) ,b:int = Query(default=None, gt=10),x_arthur: str = Header(default=None),c: Optional[int] = None):
+    if c:
+        soma = a+b + c
+    else:
+        soma = a+b
+    print(f"x_arthur , {x_arthur}")
+    return {"resultado": soma}
 if __name__ == "__main__":
     import uvicorn
     
