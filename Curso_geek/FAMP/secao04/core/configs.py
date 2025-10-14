5489fa4431f4906355f8e113064bee90f44b8412
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-14 16:17:06 -0300

    atualização

--- Curso_geek/FAMP/secao04/core/configs.py
+++ Curso_geek/FAMP/secao04/core/configs.py
@@ -1,13 +1,14 @@
 from pydantic_settings import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
+from typing import ClassVar
 
 class Setting(BaseSettings):
     """
     Configurações gerais utilizadas na aplicação
     """
     API_V1_STR:str = '/api/v1'
-    DB_URL: str = "sqlite:///./faculdade.db"
-    DBBaseModel  = declarative_base()
+    DB_URL: str = "sqlite+aiosqlite:///./faculdade.db"
+    DBBaseModel: ClassVar = declarative_base()
     
     class Config:
         case_sensitive = True
--- Curso_geek/FAMP/secao04/criar_tabelas.py
+++ Curso_geek/FAMP/secao04/criar_tabelas.py
@@ -6,4 +6,13 @@ from core.database import engine
 async def create_tables() -> None:
     import models._all_models
     
-    
+    print("Criando as tabelas no banco de dados")
+    async with engine.begin() as conn:
+        await conn.run_sync(settings.DBBaseModel.metadata.drop_all)
+        await conn.run_sync(settings.DBBaseModel.metadata.create_all)
+    print("Tabelas criadas")
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(create_tables())
