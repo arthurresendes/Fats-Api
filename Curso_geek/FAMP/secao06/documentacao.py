ab22343ae5f812561de190ef5ada4df7faecf483
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-18 15:09:04 -0300

    atualização

--- Curso_geek/FAMP/secao06/documentacao.py
+++ Curso_geek/FAMP/secao06/documentacao.py
@@ -4,8 +4,12 @@ API(Diretorio) -> Diretorio onde ficara armazendo um subdiretorio v1 que nele te
 
     - artigo.py: Nesse arquivo importamos as seguintes libs: typing(para uso no get quando queremos retornar todos artigos que seram em formato de lista): List , fastapi (ApiRouter: Seria para usar nos endpoints @, status: passar os status de requisição , httpexception: para erro juntamento com status informar o usuario, depends: utilizado para dependencias dos endpoints com o banco de dados) , sqlalchemy.ext.asyncio (AsyncSession: sessão assincrona , utilizada para passar qual tipo do banco de dados) , sqlalchemy.future(select: usado para consultas em querys internas dentro de um async with db as session), models.artigo_model (ArtigoModel: Para trazer o modelo do banco de dados com os atributos para a utilização em parametros como post , put), schemas.artigo_schema(Validar dados de entrada e formatar dados de saída (JSON).), core.deps (get_session , get_current_user: Usado para pegar o usuario atual que esta acessando/querendo acessar um endpoint). Os endpoints criados foram: @router.post("/") -> Utilizado para criar uma novo artigo ,@router.get("/") -> Utilizamos para retornar todos artigos usando o List[ArtigoSchema], @router.get("/{artigo_id}") -> Pesquisa unica onde verifica se o id existe e se existir retorna , @router.put("/{artigo_id}")-> Verifica se o id existe e faz a devida atualização,@router.delete("/{artigo_id}")-> Verifica se o id existe e faz o devido deletamento
 
-    - usuario.py
-
+    - usuario.py: Segue o mesmo molde do artigo entao não irei entrar em detalhes
+    
+    - Importante:
+        - Sempre passar o status_code e response_model(menos no delete)
+        - Sempre fazer o await db.commit() e await db.refresh() (menos no get pois é so visualização e refresh no delete nao é necessario pois não retorna nada)
+        - Sempre especificar o tipo dos parametros com :
 
 CORE ->
 
