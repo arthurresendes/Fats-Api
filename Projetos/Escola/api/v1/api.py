4c57c886f1ed58dcd2e3836c31fa3fe08217824f
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-19 15:24:27 -0300

    atualização

--- Projetos/Escola/api/v1/api.py
+++ Projetos/Escola/api/v1/api.py
@@ -3,5 +3,5 @@ from api.v1.endpoints import aluno,materia,professor
 
 api_router = APIRouter()
 api_router.include_router(aluno.router, prefix="/alunos", tags=['alunos'])
-api_router.include_router(materia.router, prefix="/materia", tags=['materias'])
+api_router.include_router(materia.router, prefix="/materias", tags=['materias'])
 api_router.include_router(professor.router, prefix="/professores", tags=['professores'])
